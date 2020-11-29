ca05af4d019f260226dc77ae34492835d92d6e52
Gabriel Wesz <gabwesz@outlook.com>
2020-11-29 17:53:49 -0300

    Adiciona comentários

--- main.pl
+++ main.pl
@@ -1,12 +1,17 @@
 :- [testcases].
 
+% Retorna o número de grupos do puzzle
 numGrupos(I, Max) :- matrizsecundaria(I, M), flatten(M, L), max_list(L, Max1), Max is Max1 + 1.
 
+% Retorna o Grupo da posição (I, J)
 getGrupoElem(Id, I, J, Elemento) :- matrizsecundaria(Id, M), nth0(I, M, L), nth0(J, L, Elemento).
 
+% Retorna o Elemento na posição (I, J) de uma matriz M
 getElemento(M, I, J, Elemento) :- nth0(I, M, L), nth0(J, L, Elemento).
 
+% Retorna o grupo identificado por NroGrupo
 getGrupo(Id, M, NroGrupo, Grupo) :- findall(Elemento, (getElemento(M, I, J, Elemento), getGrupoElem(Id, I, J, NroGrupo), Elemento \= 0), Grupo).
+
 
 getAllGrupos(Id, M, Lst) :- findall(Grupo, getGrupo(Id, M, _, Grupo), Lst).
 
@@ -14,9 +19,7 @@ getAllGrupos(Id, M, Lst) :- findall(Grupo, getGrupo(Id, M, _, Grupo), Lst).
 % Checa se há uma sequência númerica em uma lista
 eh_sequencia([])        :- !.
 eh_sequencia([_])       :- !.
-eh_sequencia([X, Y|Xs]) :- X + 1 #= Y,
-	                         eh_sequencia([Y|Xs]).
+eh_sequencia([X, Y|Xs]) :- X + 1 #= Y, eh_sequencia([Y|Xs]).
 
 % Ordena um grupo e chama eh_sequencia
-sequenciaValida(Grupo) :- sort(Grupo, GrupoOrdenado),
-                          eh_sequencia(GrupoOrdenado).
+sequenciaValida(Grupo) :- sort(Grupo, GrupoOrdenado), eh_sequencia(GrupoOrdenado).
