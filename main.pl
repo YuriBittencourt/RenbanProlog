1424fd26968f04a6e974ce83dc6d1b0836859e74
Gabriel Wesz <gabwesz@outlook.com>
2020-11-29 17:44:15 -0300

    Adiciona métodos 'eh_sequencia' e 'sequenciaValida'

--- main.pl
+++ main.pl
@@ -8,4 +8,13 @@ getElemento(M, I, J, Elemento) :- nth0(I, M, L), nth0(J, L, Elemento).
 
 getGrupo(Id, M, nroGrupo, Grupo) :- findall(Elemento, (getElemento(M, I, J, Elemento), getGrupoElem(Id, I, J, nroGrupo), Elemento \= 0), Grupo).
 
-% getM(Id, Elemento) :- matrizprimaria(Id, M), getElemento(M, 2, 0, Elemento).
+
+% Checa se há uma sequência númerica em uma lista
+eh_sequencia([])        :- !.
+eh_sequencia([_])       :- !.
+eh_sequencia([X, Y|Xs]) :- X + 1 #= Y,
+	                         eh_sequencia([Y|Xs]).
+
+% Ordena um grupo e chama eh_sequencia
+sequenciaValida(Grupo) :- sort(Grupo, GrupoOrdenado),
+                          eh_sequencia(GrupoOrdenado).
