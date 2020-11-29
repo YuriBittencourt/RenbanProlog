8ef567dad6581e415e103def62ceb45a73ceb7b1
yuriBittencourt <bittencourt.yuri@gmail.com>
2020-11-29 17:42:52 -0300

    getAllGrupos bugado

--- main.pl
+++ main.pl
@@ -6,6 +6,8 @@ getGrupoElem(Id, I, J, Elemento) :- matrizsecundaria(Id, M), nth0(I, M, L), nth0
 
 getElemento(M, I, J, Elemento) :- nth0(I, M, L), nth0(J, L, Elemento).
 
-getGrupo(Id, M, nroGrupo, Grupo) :- findall(Elemento, (getElemento(M, I, J, Elemento), getGrupoElem(Id, I, J, nroGrupo), Elemento \= 0), Grupo).
+getGrupo(Id, M, NroGrupo, Grupo) :- findall(Elemento, (getElemento(M, I, J, Elemento), getGrupoElem(Id, I, J, NroGrupo), Elemento \= 0), Grupo).
+
+getAllGrupos(Id, M, Lst) :- findall(Grupo, getGrupo(Id, M, _, Grupo), Lst).
 
 % getM(Id, Elemento) :- matrizprimaria(Id, M), getElemento(M, 2, 0, Elemento).
